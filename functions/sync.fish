ecf280aaa2e9c76ccd18da9fd3c0babfc2703371
mikkel <mikkel@willen.dk>
2024-05-28 21:26:35 +0200

    sync

--- functions/sync.fish
+++ functions/sync.fish
@@ -1,6 +1,15 @@
 function syncdoom --description 'pulls and pushes my doom directory'
+  git -C ~/.doom.d pull
   git -C ~/.doom.d add ~/.doom.d/
   git -C ~/.doom.d commit -m "sync"
   git -C ~/.doom.d push
+
+end
+
+function syncfish --description 'pulls and pushes my fish directory'
+  git -C ~/.config/fish pull
+  git -C ~/.config/fish add ~/.config/fish
+  git -C ~/.config/fish commit -m "sync"
+  git -C ~/.config/fish push
 
 end
