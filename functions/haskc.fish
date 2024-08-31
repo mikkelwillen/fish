67f4a2137aa7dcada97788916476dc458d1864af
mikkelwillen <mikkel@willen.dk>
2024-08-31 15:49:52 +0200

    sync

---
+++ functions/haskc.fish
@@ -0,0 +1,4 @@
+function haskc --wraps='stack ghc -- -w' --description 'alias haskc=stack ghc -- -w'
+  stack ghc -- -w $argv
+        
+end
---
+++ functions/haski.fish
@@ -0,0 +1,4 @@
+function haski --wraps='stack exec ghci -- -w' --description 'alias haski=stack exec ghci -- -w'
+  stack exec ghci -- -w $argv
+        
+end
